12111ca6711a338db7f8eb57cd132bb92a9afa51
AndrewSova <asovacloud@gmail.com>
2021-03-31 20:04:30 +0300

    fix: Implementing JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
-const { validationResult, check } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
 
@@ -14,67 +14,68 @@ const User = require("../../models/User");
 router.post(
     "/",
     [ 
-        check("name", "Name is required").not().isEmpty(),
-        check("email", "Please include a valid email").isEmail(),
-        check("password", "Please enter a password with 6 or more characters").isLength({ min: 6 })
+        body("name", "Name is required").not().isEmpty(),
+        body("email", "Please include a valid email").isEmail(),
+        body("password", "Please enter a password with 6 or more characters").isLength({ min: 6 })
     ],
     async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(400).json({
-            errors: errors.array()
-        })
-    }
-
-    const { name, email, password } = req.body;
-
-    try {
-      let user = await User.findOne({ email });
-
-      if(user) {
-        return res
-        .status(400)
-        .json({ erros: [{ msg: "User already exists" }] });
+      const errors = validationResult(req);
+      if(!errors.isEmpty()) {
+          return res.status(400).json({
+              errors: errors.array()
+          })
       }
 
-      const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pg",
-        d: "mm"
-      });
+      const { name, email, password } = req.body;
 
-      user = new User({
-        name,
-        email,
-        avatar,
-        password
-      });
+      try {
+        let user = await User.findOne({ email });
 
-      const salt = await bcrypt.genSalt(10);
-
-      user.password = await bcrypt.hash(password, salt);
-
-      await user.save();
-
-      const payload = {
-        user: {
-          id: user.id
+        if(user) {
+          return res
+          .status(400)
+          .json({ erros: [{ msg: "User already exists" }] });
         }
+
+        const avatar = gravatar.url(email, {
+          s: "200",
+          r: "pg",
+          d: "mm"
+        });
+
+        user = new User({
+          name,
+          email,
+          avatar,
+          password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+          user: {
+            id: user.id
+          }
+        }
+
+        jwt.sign(
+          payload,
+          config.get("jwtToken"),
+          { expiresIn: 360000 },
+          (err, token) => {
+            if(err) throw err;
+            res.json({ token });
+          }
+        );
+      } catch {
+        console.error(err.message);
+        res.status(500).send("Server error");
       }
-
-      jwt.sign(
-        payload,
-        config.get("jwtToken"),
-        { expiresIn: 360000 },
-        (err, token) => {
-          if(err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch {
-      console.error(err.message);
-      res.status(500).send("Server error");
     }
-});
+);
 
 module.exports = router;
