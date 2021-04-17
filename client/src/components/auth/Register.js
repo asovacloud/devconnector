cb3ace17ab0972d4278ecb7cbe596d8f4d18d4fe
AndrewSova <asovacloud@gmail.com>
2021-04-17 11:05:19 +0300

    add register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -15,12 +16,31 @@ const Register = () => {
     [e.target.name]: e.target.value
   })
 
-  const handleOnSubmit = e => {
+  const handleOnSubmit = async e => {
     e.preventDefault();
     if(password !== password2) {
       console.log("Passwords do not match");
     } else {
-      console.log(formData);
+      const newUser = {
+        name,
+        email,
+        password
+      }
+
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+
+        const body = JSON.stringify(newUser);
+
+        const res = await axios.post("api/users", body, config);
+        console.log(res.data);
+      } catch (err) {
+        console.error(err.response.data);
+      }
     }
   }
 
