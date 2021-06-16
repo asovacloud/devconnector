a50ce9a73cdace6dfbd9f78d640f2d2a604672ee
AndrewSova <asovacloud@gmail.com>
2021-06-16 09:30:58 +0300

    feature: Starting On The Profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getProfileById } from "../../actions/profile";
@@ -15,7 +16,24 @@ const Profile = ({
   }, [getProfileById]);
 
   return (
-    <>{profile === null || loading ? <Spinner /> : <>profile content</>}</>
+    <>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Link to="/profiles" className="btn btn-light">
+            Back To Profile
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+        </>
+      )}
+    </>
   );
 };
 
